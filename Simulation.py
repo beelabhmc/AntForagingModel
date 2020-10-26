cdf31719c66936bced251fe943a700d77b79433c
Miguel Velez <miguelvelez@Miguels-MacBook-Pro.local>
2020-10-26 16:04:28 -0400

    using vals params

--- Simulation.py
+++ Simulation.py
@@ -10,20 +10,20 @@ import sys
 # PARAMETERS 
 
 runs   = 1               # How many times we run the simulation
-J      = 5               # Number of food sources (aka, number of trails to food sources)
-N      = 10000           # Total number of ants
-alpha  = 91.7            # Per capita rate of spontaneous discoveries
-s      = 81.2            # Per capita rate of ant leaving trail per distance
-gamma1 = 11.9            # Range of foraging scouts
-gamma2 = 5.81*(10**(-6)) # Range of recruitment activity
-gamma3 = 1.91*(10**(-3)) # Range of influence of pheromone
-K      = 8.11*(10**(-3)) # Inertial effects that may affect pheromones
-n1     = 1.2             # Individual ant's contribution to rate of recruitment (orig. eta1)
-n2     = 0.99            # Pheromone strength of trail (originally eta2)
+J      = 1               # Number of food sources (aka, number of trails to food sources)
+N      = 5000            # Total number of ants
+alpha  = 9.170414e+01    # Per capita rate of spontaneous discoveries
+s      = 8.124702e+01    # Per capita rate of ant leaving trail per distance
+gamma1 = 1.186721e+01    # Range of foraging scouts
+gamma2 = 5.814424e-06    # Range of recruitment activity 
+gamma3 = 1.918047e-03    # Range of influence of pheromone 
+K      = 8.126483e-03    # Inertial effects that may affect pheromones 
+n1     = 1.202239e+00    # Individual ant's contribution to rate of recruitment (orig. eta1)
+n2     = 9.902102e-01    # Pheromone strength of trail (originally eta2)
 
-Qmin = 0                 # Minimum & Maximum of Quality uniform distribution
+Qmin = 0                # Minimum & Maximum of Quality uniform distribution
 Qmax = 20
-Dmin = 0                 # Minimum & Maximum of Distance uniform distribution
+Dmin = 0                # Minimum & Maximum of Distance uniform distribution
 Dmax = 1
 
 betaB  = np.zeros(J)     # How much each ant contributes to recruitment to a trail
@@ -33,7 +33,7 @@ betaS  = np.zeros(J)     # Relationship btwn pheromone strength of a trail & its
 
 start = 0.0
 stop  = 1500.0
-step  = 5.0
+step  = 0.005
 tspan = np.arange(start,stop+step,step)
 
 # INITIAL CONDITIONS
@@ -50,8 +50,7 @@ def dx_dt(x,t):
     on each of the J trails. (Eqn i corresponds to food source i)
     """
     system = np.zeros(J)
-    for i in range(J):
-        system[i] = (alpha* np.exp(-gamma1*D[i]) + (gamma2/D[i])*betaB[i]*x[i])*(N-sum(x)) - (s*D[i]*x[i])/(K+ (gamma3/D[i])*betaS[i]*x[i])
+    system = (alpha* np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x)
     return system
 
 # RUNS AND MODEL OUTPUT
@@ -63,24 +62,20 @@ weight_avg_D = np.zeros(runs)
 prop_committed_ants    = np.zeros(len(tspan))   # Proportion of committed ants 
 prop_noncommitted_ants = np.zeros(len(tspan))   # Proportion of non committed ants 
 for w in range(runs):
-    Q = np.random.uniform(Qmin,Qmax,J)      # Choose each trail's quality from uniform distrib   [10]   
-    D = np.random.uniform(Dmin,Dmax,J)      # Choose each trail's distance from uniform distrib  [0.55] 
-    print('Q: ',Q)
-    print('D: ',D)
-    for i in range(J):
-        betaB[i] = Q[i] * n1                # Each trail has different betas, so they're calculated here
-        betaS[i] = Q[i] * n2
+    Q = 20      # Choose each trail's quality from uniform distrib   np.random.uniform(Qmin,Qmax,J)   
+    D = 0.55    # Choose each trail's distance from uniform distrib  np.random.uniform(Dmin,Dmax,J)   
+    betaB = n1 * Q
+    betaS = n2 * Q
+
     xs = odeint(dx_dt, x0, tspan)           # Solve the system, Columns: trail (food source), Rows: time step
     final_time[w,:] = xs[-1,:]              # 2D array of the number of ants on each trail at the last timestep. Columns: trail (food source), Rows: runs.
-    
-    for i in range(J):
-        density[w,i] = xs[-1,i]/D[i]                         
+                            
     weight_avg_Q[w]  = sum((final_time[w,:] * Q)/N)  # Weighted average of quality (selected.Q in R)
     weight_avg_D[w]  = sum((final_time[w,:] * D)/N)  # Weighted average of distance (selected.D in R)
     
     for t in range(len(tspan)):
         prop_committed_ants[t]    = sum(xs[t,:]/N)
-        prop_noncommitted_ants[t] = 1 - prop_committed_ants[t]
+    prop_noncommitted_ants = 1 - prop_committed_ants
     
 
 
