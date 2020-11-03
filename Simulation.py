ae6890bb46876e2c57469f887c7eeeeea1a9d078
fletcher <67565911+fl-hat@users.noreply.github.com>
2020-11-02 20:39:20 -0500

    Added comments and csv export

--- Simulation.py
+++ Simulation.py
@@ -10,7 +10,7 @@ import sys
 
 # PARAMETERS 
 
-runs   = 10            # How many times we run the simulation
+runs   = 10              # How many times we run the simulation
 J      = 5               # Number of food sources (aka, number of trails to food sources)
 N      = 5000            # Total number of ants
 alpha  = 9.170414e+01    # Per capita rate of spontaneous discoveries
@@ -35,7 +35,7 @@ betaS  = np.zeros(J)     # Relationship btwn pheromone strength of a trail & its
 start = 0.0
 stop  = 50.0
 step  = 0.005
-tspan = np.arange(start,stop+step,step)
+tspan = np.arange(start, stop+step, step)
 
 # INITIAL CONDITIONS
 
@@ -58,54 +58,60 @@ def dx_dt(x,t,Q,D,betaB,betaS):
 
 density      = np.zeros([runs,J])
 final_time   = np.zeros([runs,J])
-weight_avg_Q = np.zeros(runs)
-weight_avg_D = np.zeros(runs)
+weight_avg_Q = np.zeros(runs)                   # Sum of (# of ants on a trail * its quality)
+weight_avg_D = np.zeros(runs)                   # Sum of (# of ants on a trail * its distance)
 prop_committed_ants    = np.zeros(len(tspan))   # Proportion of committed ants 
 prop_noncommitted_ants = np.zeros(len(tspan))   # Proportion of non committed ants 
 
 def simulation():
     for w in range(runs):
         Q = np.random.uniform(Qmin,Qmax,J)      # Choose each trail's quality from uniform distrib      
-        D = np.random.uniform(Dmin,Dmax,J)    # Choose each trail's distance from uniform distrib     
+        D = np.random.uniform(Dmin,Dmax,J)      # Choose each trail's distance from uniform distrib     
         betaB = n1 * Q
         betaS = n2 * Q
 
-        xs = odeint(dx_dt, x0, tspan, args=(Q,D,betaB,betaS))           # Solve the system, Columns: trail (food source), Rows: time step
-        final_time[w,:] = xs[-1,:]              # 2D array of the number of ants on each trail at the last timestep. Columns: trail (food source), Rows: runs.
+        xs = odeint(dx_dt, x0, tspan, args=(Q,D,betaB,betaS))   # Solves the system. Columns: trail (food source), Rows: time step
+        final_time[w,:] = xs[-1,:]              # 2D array of # of ants on each trail at the last timestep. Columns: trail (food source), Rows: runs.
     
         weight_avg_Q[w]  = sum((final_time[w,:] * Q)/N)  # Weighted average of quality (selected.Q in R)
         weight_avg_D[w]  = sum((final_time[w,:] * D)/N)  # Weighted average of distance (selected.D in R)
 
+# PARAMETER SWEEPING
 
-param            = np.linspace(9,15,3)
-param_values     = []
-weight_avg_Q_tot = []
+# Here we choose one parameter to vary (param), specifying a range and number of values to try.
+# • We are exporting weighted avg info in a tidy csv, so we're creating 3 different lists
+#   that will be turned into columns in a dataframe.
+# • We keep track of which value was used in a particular run (row) with the param_values list.
+
+param            = np.linspace(9,15,3)          # (start, stop, # samples). Creates array of param values to test.
+param_values     = []                           # specifies which value's used for param during each chunk of sim runs. used in df.
+weight_avg_Q_tot = []                           # list of all the Q weighted avg values from all sim for all tested values of param
 weight_avg_D_tot = []
-for p in range(len(param)):
+for p in range(len(param)):                     # for each value of param...
     gamma1 = param[p]
-    param_values += ([param[p]] * runs)
     simulation()
-    weight_avg_Q_tot += list(weight_avg_Q)
+    param_values += ([param[p]] * runs)         # add param value (once for each run) to list of param values
+    weight_avg_Q_tot += list(weight_avg_Q)      # add onto list of quality weighted averages with values for this set of runs
     weight_avg_D_tot += list(weight_avg_D)
 
 #=================================================================================================#
 
 # PROCESSING DATA
 
-Q_bins = np.arange(Qmin,Qmax+0.5,0.5)
+Q_bins = np.arange(Qmin,Qmax+0.5,0.5)                           # note that the step needs to be added to Qmax
 Q_hist,Q_edges = np.histogram(weight_avg_Q, bins = Q_bins)
 #print('Q_hist: ',Q_hist)
 #print('Q_edges: ',Q_edges)
 
-Q_distr = np.zeros(len(Q_bins))
+Q_distr = np.zeros(len(Q_bins))     # Q distribution
 Q_distr = Q_hist/(runs) 
 
-D_bins = np.arange(Dmin,Dmax+0.01,0.01)
-D_hist,D_edges = np.histogram(weight_avg_D, bins = D_bins)
+D_bins = np.arange(Dmin,Dmax+0.01,0.01)                         # note that there will be a different # of Q & D bins
+D_hist,D_edges = np.histogram(weight_avg_D, bins = D_bins)      # bc their values are at different scales
 #print('D_hist: ',D_hist)
 #print('D_edges: ',D_edges)
 
-D_distr = np.zeros(len(D_bins))
+D_distr = np.zeros(len(D_bins))     # D distribution
 D_distr = D_hist/(runs)
 
 
@@ -113,13 +119,18 @@ D_distr = D_hist/(runs)
 
 # CREATING CSV
 
+# Create dataframe
 d = {'Param Values': param_values, 'WeightedQ': weight_avg_Q_tot,'WeightedD': weight_avg_D_tot}
 df = pd.DataFrame(data=d)
 print(df)
 
+# Export
+df.to_csv(r'/Users/nfn/Desktop/Ants/gamma1_df_nov2.csv', index = False) # Fletcher's path
+#df.to_csv( INSERT PATH , index = False)                                # Miguel's path
+
 #=================================================================================================#
 
-# Plotting
+# PLOTTING
 
 plt.rc('font', family='serif')
 
@@ -160,14 +171,14 @@ plt.rc('font', family='serif')
 #plt.xlabel('weighted D',fontsize=15)
 #plt.ylabel('count',fontsize=15)
 
-# Plotting Probability distribution of weighted average of quality
+# Plotting Probability distribution of quality weighted average
 plt.figure()
 plt.bar(Q_bins[:-1], Q_distr, width = 0.5, color='#0504aa',alpha=0.7)
 plt.title('Distribution Weighted average of Quality',fontsize=15)
 plt.xlabel('Weighted Average of Quality',fontsize=15)
 plt.ylabel('Probability',fontsize=15)
 
-# Plotting Probability distribution of weighted average of distance
+# Plotting Probability distribution of distance weighted average
 plt.figure()
 plt.bar(D_bins[:-1], D_distr, width = 0.01, color='#0504aa',alpha=0.7)
 plt.title('Distribution Weighted average of Distance',fontsize=15)
