19a2259d3c5d50774aba85325e067745c8a36003
dhpitt <david.h.pitt@gmail.com>
2021-02-26 11:50:51 -0800

    convergence stop added

--- sim-ivp.py
+++ sim-ivp.py
@@ -12,7 +12,7 @@ import sys
 # PARAMETERS 
 
 runs   = 1             # How many times we run the simulation
-J      = 5               # Number of food sources (aka, number of trails to food sources)
+J      = 10               # Number of food sources (aka, number of trails to food sources)
 N      = 5000            # Total number of ants
 alpha  = 9.170414e+01    # Per capita rate of spontaneous discoveries
 s      = 8.124702e+01    # Per capita rate of ant leaving trail per distance
@@ -30,6 +30,10 @@ Dmax = 0.5
 
 betaB  = np.zeros(J)     # How much each ant contributes to recruitment to a trail
 betaS  = np.zeros(J)     # Relationship btwn pheromone strength of a trail & its quality
+
+# SOLVER BEHAVIOR
+
+convergence_range = 0.1 # range for dx_dt max to signal convergence
 
 # TIME 
 
@@ -51,25 +55,20 @@ all_params_vals   =   [runs, J, N, alpha, s, gamma1, gamma2, gamma3, K, n1, n2, 
 
 # SYSTEM OF EQUATIONS
 
-def dx_dt(x,t,Q,D,betaB,betaS):
-    # TODO: REWRITE DXDT AS A SYSTEM THAT MAKES SOLVEIVP HAPPY
-    """
-    Creates a list of J equations describing the number of ants
-    on each of the J trails. (Eqn i corresponds to food source i)
-    """
-    #system = np.zeros(J)
-    print(betaB)
-    print(betaS)
-    print(x)
-    # the bit sum(x) is what's making solve ivp angry. he wants an array but
-    # somehow x is being passed in as float.
-    system = [(alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB[i]*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS[i]*x) for i in range(J)]
-    return system
-
+# rhs returns dx_dt for our array x.
 def rhs(t,x,D,betaB,betaS):
-    dx = (alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x)
-    return [d if d > 0 else 0 for d in dx] # set the floor of the output to 0
+    x = [y if y > 0 else 0 for y in x]
+    return [(alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x)] # set the floor of the output to 0
+    #return (alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x) # set the floor of the output to 0
 
+# Function that checks for convergence
+def convergence(t,x,D,betaB,betaS):
+    m = np.amax([abs(y) for y in rhs(t,x,D,betaB,betaS)])
+    if m < convergence_range:
+        return 0 
+    else:
+        return m
+convergence.terminal = True
 
 def jacobian(x,t,Q,D,betaB,betaS):
     jac_matrix = np.zeros([J,J]) 
@@ -108,10 +107,13 @@ def ivp_simulation():
         betaB = n1 * Q
         betaS = n2 * Q
 
-        sol = solve_ivp(rhs,[start,stop],x0,args=(D,betaB,betaS),dense='true', method = 'LSODA')
-    return(sol,[list(Q),list(D)])
+        #sol = solve_ivp(rhs,[start,stop],x0,args=(D,betaB,betaS),dense='true',method = 'LSODA')
+        sol = solve_ivp(rhs,[start,stop],x0,events=convergence,args=(D,betaB,betaS),method = 'LSODA')
+    #return(sol,[list(Q),list(D)])
+    return(sol)
 
-sol,initials = ivp_simulation() #this is in a special type that gives you extra info if you print it
+sol = ivp_simulation() #this is in a special type that gives you extra info if you print it
+print(sol)
 # sol.t is timesteps
 # sol.y is the solutions: one row for each trail, timesteps are cols
 # this is the transpose of odeint's output
@@ -120,18 +122,16 @@ testruns = 100
 
 def find_negatives():
     negative_testruns = np.zeros(testruns)  
-    negative_ics = []
     for i in range(testruns):
         isnegative = 0
         #print(int(np.floor(i*100/testruns)), "% 🐜") # Progress bar
-        sol,ic_list = ivp_simulation()
+        sol = ivp_simulation()
         # sol.y is the solutions: one row for each trail, timesteps are cols
         # reshape array so it's just one list
         flat_soly = list(sol.y.reshape(-1))
         for k in flat_soly:
             if(k<0):
                 isnegative = 1
-                negative_ics.append(ic_list)
                 break
         negative_testruns[i] = isnegative
     percent_negative = sum(negative_testruns) / testruns
@@ -140,22 +140,24 @@ def find_negatives():
     print("Stop : ", stop)
     print("Step : ", step)
     print("Per¢ : ", int(np.ceil(percent_negative*100)), "%")
-    return(percent_negative,negative_ics)
+    return(percent_negative)
 
 #print(len(sol.t))
 #print(len(sol.y))
 
 #print(find_negatives())
-negPercent,negICs = find_negatives()
+#negPercent,negICs = find_negatives()
 # Create a dense solution
 #t = np.linspace(0,50,1000)
+t = sol.t
+y = sol.y
 #y = sol.sol(t)
 
 # Visualize
-qspace = [x[0] for x in negICs]
-dspace = [x[1] for x in negICs]
+#qspace = [x[0] for x in negICs]
+#dspace = [x[1] for x in negICs]
+
 ax = plt.subplot()
-ax.set(xlabel='Q',ylabel='D')
-#ax.plot(t,y.T)
-ax.scatter(qspace,dspace)
+ax.set(xlabel='t',ylabel='Number of ants')
+ax.plot(t,y.T)
 plt.show()
