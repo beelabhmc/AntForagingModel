bf3e34a4fa9b17006088e07b56f34ebffa1d367b
dhpitt <david.h.pitt@gmail.com>
2021-04-09 11:53:16 -0700

    Working on convergence data

--- sim-ivp.py
+++ sim-ivp.py
@@ -1,10 +1,15 @@
+from numpy.lib.shape_base import dstack
+from numpy.lib.ufunclike import _dispatcher
 import scipy as sc
 import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
+import scipy.stats as stat
 import matplotlib.pyplot as plt
 import sys
+from tqdm import tqdm
+from matplotlib.pyplot import get_cmap
 #np.set_printoptions(threshold=sys.maxsize) # This allows to you to print large arrays without truncating them
 
 #=================================================================================================#
@@ -12,7 +17,7 @@ import sys
 # PARAMETERS 
 
 runs   = 1             # How many times we run the simulation
-J      = 10               # Number of food sources (aka, number of trails to food sources)
+J      = 5               # Number of food sources (aka, number of trails to food sources)
 N      = 5000            # Total number of ants
 alpha  = 9.170414e+01    # Per capita rate of spontaneous discoveries
 s      = 8.124702e+01    # Per capita rate of ant leaving trail per distance
@@ -26,19 +31,20 @@ n2     = 9.902102e-01    # Pheromone strength of trail (originally eta2)
 Qmin = 0                 # Minimum & Maximum of Quality uniform distribution
 Qmax = 20
 Dmin = 0                 # Minimum & Maximum of Distance uniform distribution
-Dmax = 0.5
+#Dmax = 0.5
+Dmax = 1
 
 betaB  = np.zeros(J)     # How much each ant contributes to recruitment to a trail
 betaS  = np.zeros(J)     # Relationship btwn pheromone strength of a trail & its quality
 
 # SOLVER BEHAVIOR
 
-convergence_range = 0.1 # range for dx_dt max to signal convergence
+convergence_range = 1 # range for dx_dt max to signal convergence
 
 # TIME 
 
 start = 0.0
-stop  = 50.0            # One of our goals is to change this cutoff to reflect convergence
+stop  = 255.0            # One of our goals is to change this cutoff to reflect convergence
 step  = 0.005
 tspan = np.arange(start, stop+step, step)
 
@@ -57,13 +63,14 @@ all_params_vals   =   [runs, J, N, alpha, s, gamma1, gamma2, gamma3, K, n1, n2, 
 
 # rhs returns dx_dt for our array x.
 def rhs(t,x,D,betaB,betaS):
-    x = [y if y > 0 else 0 for y in x]
+    #x = [y if y > 0 else 0 for y in x]
+    x = [1/2*(y+abs(y)) for y in x] # suggestion to constrain x given by Bernoff and Weinburd
     return [(alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x)] # set the floor of the output to 0
     #return (alpha * np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x) # set the floor of the output to 0
 
 # Function that checks for convergence
 def convergence(t,x,D,betaB,betaS):
-    m = np.amax([abs(y) for y in rhs(t,x,D,betaB,betaS)])
+    m = 5*np.amax([abs(y) for y in rhs(t,x,D,betaB,betaS)]) # check if one ant total is changing trails
     if m < convergence_range:
         return 0 
     else:
@@ -93,14 +100,13 @@ dif_btwn_avgs_D = np.zeros(runs)
 prop_committed_ants    = np.zeros(len(tspan))   # Proportion of committed ants (committed =  on a trail)
 prop_noncommitted_ants = np.zeros(len(tspan))   # Proportion of non-committed ants 
 
-
 def ivp_simulation():
     # This version is a test that uses SciPy solve_ivp instead of odeint
     # with the eventual goal of using solve_ivp's event detector to stop
     # the simulation on convergence. 
-
+    data_out = []
     for w in range(runs):
-        print(int(np.floor(w*100/runs)), "% 🐜") # Progress bar
+        #print(int(np.floor(w*100/runs)), "%") # Progress bar
         x0 = np.zeros(J)
         Q = np.random.uniform(Qmin,Qmax,J)      # Choose each trail's quality from uniform distribution      
         D = np.random.uniform(Dmin,Dmax,J)      # Choose each trail's distance from uniform distribution     
@@ -109,16 +115,21 @@ def ivp_simulation():
 
         #sol = solve_ivp(rhs,[start,stop],x0,args=(D,betaB,betaS),dense='true',method = 'LSODA')
         sol = solve_ivp(rhs,[start,stop],x0,events=convergence,args=(D,betaB,betaS),method = 'LSODA')
-    #return(sol,[list(Q),list(D)])
-    return(sol)
+        #data_out.append([sol,[list(Q),list(D)]])
+    if list(sol.t_events[-1]):
+        return sol,[list(Q),list(D)],list(sol.t_events[-1])[0]
+    else:
+        return sol,[list(Q),list(D)],255
+
+    #return sol
 
 sol = ivp_simulation() #this is in a special type that gives you extra info if you print it
-print(sol)
+#print(sol)
 # sol.t is timesteps
 # sol.y is the solutions: one row for each trail, timesteps are cols
 # this is the transpose of odeint's output
 
-testruns = 100
+testruns = 1000
 
 def find_negatives():
     negative_testruns = np.zeros(testruns)  
@@ -142,6 +153,30 @@ def find_negatives():
     print("Per¢ : ", int(np.ceil(percent_negative*100)), "%")
     return(percent_negative)
 
+def qd_convergence_comparison(numIter):
+    qs = np.zeros([J,testruns])
+    ds = np.zeros([J,testruns])
+    dframe = np.zeros([testruns,testruns,J])
+    timeToConverge = []
+    finalVals = []
+    print("Generating convergence data...")
+    for i in tqdm(range(numIter)):
+        sol,qdArray,conTime = ivp_simulation()
+        qdArray[0] = stat.zscore(qdArray[0])
+        qdArray[1] = stat.zscore(qdArray[1])
+        
+
+        end = list(sol.y[:,-1])
+        trailChoice = end.index(max(end))
+
+
+        finalVals.append(end.index(max(end)))
+        timeToConverge.append(conTime)
+    print("done!")
+    return qs,ds,timeToConverge,finalVals
+
+
+#print(sol)
 #print(len(sol.t))
 #print(len(sol.y))
 
@@ -149,15 +184,35 @@ def find_negatives():
 #negPercent,negICs = find_negatives()
 # Create a dense solution
 #t = np.linspace(0,50,1000)
-t = sol.t
-y = sol.y
+#t = sol.t
+#y = sol.y
+#convergence_time = sol.t_events[-1]
+#print(convergence_time)
 #y = sol.sol(t)
 
+qspace,dspace,Ts,fV = qd_convergence_comparison(testruns)
+
+
+print(fV)
+#print(Ts)
+#print(qspace)
 # Visualize
 #qspace = [x[0] for x in negICs]
 #dspace = [x[1] for x in negICs]
-
 ax = plt.subplot()
+ax.set(xlabel='Quality (z-score)',ylabel='Distance (z-score)')
+ax.set(title="Relative quality/distance of chosen food source vs. convergence time")
+# color = [[str(t/255.) for _ in range(J)] for t in Ts] # plot all q and d
+color = [str(t/255.) for t in Ts] # plot only max val's q and d
+for i in range(testruns):
+    #ax.scatter(qspace[:,i],dspace[:,i],c=color[i],cmap='viridis')
+    plt.set_cmap('viridis')
+    ax.scatter(qspace[fV[i],i],dspace[fV[i],i],c=color[i],s=5)
+#plt.colorbar(mappable=color)
+'''
 ax.set(xlabel='t',ylabel='Number of ants')
+ax.set(title="Number of ants over time")
 ax.plot(t,y.T)
+'''
+#ax.legend()
 plt.show()
