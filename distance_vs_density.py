16b66490c71406cf948314d23d7ae08cda6e4eca
Zachary Traul <ztraul@hmc.edu>
2023-02-03 18:01:07 -0800

    added fit to exponential distribution, one parameter sweep functions

--- distance_vs_density.py
+++ distance_vs_density.py
@@ -1,4 +1,5 @@
 import scipy as sc
+from scipy import stats
 import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
@@ -8,25 +9,30 @@ import os
 
 # PARAMETERS
 
-runs   = 10000           # How many times we run the simulation
+#Parameters for the simulation
+runs   = 500             # How many times we run the simulation
 J      = 5               # Number of food sources (aka, number of trails to food sources)
 N      = 10000           # Total number of ants
-alpha  = 0.75            # Per capita rate of spontaneous discoveries
-s      = 3.5             # Per capita rate of ant leaving trail per distance
-gamma1 = 0.2             # Range of foraging scouts
-gamma2 = 0.021           # Range of recruitment activity
-gamma3 = 0.021           # Range of influence of pheromone
-K      = 1               # Inertial effects that may affect pheromones
-n1     = 20              # Individual ant's contribution to rate of recruitment (orig. eta1)
-n2     = 20              # Pheromone strength of trail (originally eta2)
+
+#Parameters from the equation, accessed by name
+p    = {
+"alpha"  : 0.75            # Per capita rate of spontaneous discoveries
+"s"      : 3.5             # Per capita rate of ant leaving trail per distance
+"gamma1" : 0.2             # Range of foraging scouts
+"gamma2" : 0.021           # Range of recruitment activity
+"gamma3" : 0.021           # Range of influence of pheromone
+"K"      : 1               # Inertial effects that may affect pheromones
+"n1"     : 20              # Individual ant's contribution to rate of recruitment (orig. eta1)
+"n2"     : 20              # Pheromone strength of trail (originally eta2)       
+        }
 
 Qmin = 0
 Qmax = 20
 Dmin = 0
 Dmax = 60
 
-betaB  = np.zeros(J)     # How much each ant contributes to recruitment to a trail
-betaS  = np.zeros(J)     # Relationship btwn pheromone strength of a trail & its quality
+betaB  = np.zeros(p["J"])     # How much each ant contributes to recruitment to a trail
+betaS  = np.zeros(p["J"])     # Relationship btwn pheromone strength of a trail & its quality
 
 # TIME
 
@@ -39,11 +45,6 @@ tspan = np.arange(start, stop+step, step)
 
 x0 = np.zeros(J)         # We start with no ants on any of the trails
 
-# LIST OF PARAMETERS    (for exporting/reproducing results)
-
-all_params_names  =   ["runs", "J", "N", "alpha", "s", "gamma1", "gamma2", "gamma3", "K", "n1", "n2", "Qmin", "Qmax", "Dmin", "Dmax", "start", "stop", "step", "tspan", "x0"]
-all_params_vals   =   [runs, J, N, alpha, s, gamma1, gamma2, gamma3, K, n1, n2, Qmin, Qmax, Dmin, Dmax, start, stop, step, tspan, x0]
-
 # SYSTEM OF EQUATIONS
 
 def dx_dt(x,t,Q,D,betaB,betaS):
@@ -52,29 +53,73 @@ def dx_dt(x,t,Q,D,betaB,betaS):
         on each of the J trails. (Eqn i corresponds to food source i)
         """
         system = np.zeros(J)
-        system = (alpha* np.exp(-gamma1*D) + (gamma2/D)*betaB*x)*(N-sum(x)) - (s*D*x)/(K+ (gamma3/D)*betaS*x)
+        system = (p["alpha"]* np.exp(-p["gamma1"]*D) + (p["gamma2"]/D)*betaB*x)*(N-sum(x)) - (p["s"]*D*x)/(p["K"]+ (p["gamma3"]/D)*betaS*x)
         return system
 
 
 # RUNS AND MODEL OUTPUT
 
-final_time = np.zeros([runs, J])
-weight_avg_D = np.zeros(runs) # Sum of (# of ants on a trail * its distance)/(total number of trails)
 
 def simulation():
+
+    final_time = np.zeros(runs, J)
+    weight_avg_D = np.zeros(runs) # Sum of (# of ants on a trail * its distance)/(total number of trails)
     for w in range(runs):
         print(f"Run {w} of {runs} is running.\r", end="")
         Q = np.random.uniform(Qmin, Qmax, J)         #Choose each trail's quality from uniform dist
         D = np.random.uniform(Dmin, Dmax, J)         #Choose each trail's distance from uniform dist
 
-        betaB = n1 * Q
-        betaS = n2 * Q
+        betaB = p["n1"] * Q
+        betaS = p["n2"] * Q
 
         xs = odeint(dx_dt, x0, tspan, args=(Q,D,betaB,betaS)) #Solves the system. Columns: trail, Rows: time step
         final_time[w,:] = xs[-1,:]
         weight_avg_D[w] = sum((final_time[w,:] * D)/N)
 
-simulation()
-weight_avg_data = pd.DataFrame(weight_avg_D)
-weight_avg_data.columns = ['waD']
-weight_avg_data.to_csv(f'{os.path.dirname(__file__)}/results/weight_avg_test.csv',index=False)
+    return weight_avg_D
+
+def plot_fit(b, weight_avg):
+    """
+    plot fit takes in the list of weighted averages, plots a histogram of their distribution,
+        and then plots the fitted distribution calculated by get_fit over them, mostly used for
+        sanity-checking.
+    """
+    # Make our plot
+    plt.rcParams['text.usetex'] = True                      #Comment out this line if you don't want to render latex.
+    fig, ax = plt.subplots(figsize=(6,4), tight_layout=True)
+
+    # Plot the normalized average weight histogram
+    counts, bins = np.histogram(weight_avg)
+    ax.stairs(100*counts/((bins[1]-bins[0])*sum(counts)), bins)
+
+    # Plot the fitted distribution on top
+    x_fitted = np.linspace(np.min(bins), np.max(bins), 100)
+    y_fitted = 100 * (1/b) * np.exp((-1/b) * x_fitted) 
+    ax.plot(x_fitted, y_fitted)
+
+    ax.set_xlabel("Weighted Average of Distance to Food Source")
+    ax.set_ylabel(r'$10^{-2}$ Density')
+    plt.show()
+
+def get_fit(weight_avg):
+    """
+    get_fit takes in a list of weighted averages generated by simulation and returns lambda,
+        which is the parameter of an exponential distribution
+    """
+    b = stats.expon.fit(weight_avg, floc=0)[1]
+    print(f"beta: {b}")
+    return b
+
+def sweep_one_fit(param, values):
+    """ sweep_one_rtakes in the name of a parameter param string (ex: "alpha") and a list of the values
+        to run a sweep on.
+        Returns two arrays, the first containing the values and the second the resulting b value.
+    """
+    b_list = []
+    for val in values:
+        p[param] = val
+        b_list.append(simulation)
+    return values, b_list
+
+sim = simulation()
+plot_fit(get_fit(sim), sim)
